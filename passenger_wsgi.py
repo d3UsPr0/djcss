4dcf37baf02f03bc183fdd14bf30216ea3eec23d
d3uspr0 <deuc60@gmail.com>
2025-07-19 14:49:51 +0300

    Ready for deployment

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,12 +5,12 @@ import sys
 project_home = '/home/drjohnch/djcss'
 
 # Add your project to the system path
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
+#if project_home not in sys.path:
+#sys.path.insert(0, project_home)
 
 # Set environment variable for Django settings module
-os.environ['DJANGO_SETTINGS_MODULE'] = 'djcss.settings'
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'djcss.settings'
 
 # WSGI application
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from django.core.wsgi import application
+#application = get_wsgi_application()
