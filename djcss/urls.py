f481010218a398430339328a01644544f9f52ca1
d3uspr0 <deuc60@gmail.com>
2025-07-20 12:05:28 +0300

    Add cPanel deployment config

--- djcss/urls.py
+++ djcss/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('', include('web.urls')),
     path('ckeditor5/', include('django_ckeditor_5.urls')),
     
-
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
